c333c1fc2fafdc3a2cdddd0145b6bdc87be94c4d
smritinaik <148971067+smritinaik@users.noreply.github.com>
2024-12-09 21:28:02 +0530

    scroll edit
    scroll edit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,16 @@ export default function Home() {
       <main className="flex items-center justify-center w-full h-full flex-col px-14">
       <RecentProjects />
       <Screfft/>
-      <RecentProjects />
+      <div
+            className='h-full md:h-[90rem] w-full rounded-md flex flex-col   relative oberflow-hidden mx-auto py-10 md:py-0'
+        >
+            <div className='p-4 relative z-10 w-full text-center'>
+                <h1
+                    className='mt-20 md:mt-0 text-5xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-900'
+                >Designed And Developerd By SMRITI NAIK  </h1>
+            </div>
+
+        </div>
       </main>
     </>
   );

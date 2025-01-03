92dc9f7fd57add8b825343411f3ee2695dfb509a
smritinaik <148971067+smritinaik@users.noreply.github.com>
2025-01-03 18:02:51 +0530

    edt
    edt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,27 +9,16 @@ import UnderSpck from "@/components/UnderSpck";
 export default function Home() {
   return (
     <>
-
-      {/* <HeroSection /> */}
       <SparklesPreview />
       <RecentProjects />
       <Screfft />
-
       <main className="flex items-center justify-center w-full h-full flex-col px-9">
         <div className='h-full md:h-[7rem] w-full rounded-md flex flex-col   relative oberflow-hidden mx-auto py-10 md:py-0'>
         </div>
       </main>
-
       <Bento />
       <UnderSpck/>
-
-      {/* <main className="flex items-center justify-center w-full h-full flex-col px-9">
-        <div className='h-full md:h-[7rem] w-full rounded-md flex flex-col   relative oberflow-hidden mx-auto py-10 md:py-0'>
-        </div>
-      </main> */}
-
       <Footer />
-
     </>
   );
 }
